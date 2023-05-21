5489ebaf08f76a6ec44196eb3403a611a8589e09
Hafiz Abdur Rahman <88561828+HAbdurRahman@users.noreply.github.com>
2023-05-21 21:10:07 +0500

    Create C++ even or odd program.cpp

---
+++ C++ even or odd program.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a Number" << endl;
+    cin >> n;
+    if (n%2==0)
+    {
+     cout << "Number is Even" << endl;
+    }
+    else
+    {
+        cout << "Number is Odd" << endl;
+    }
+    return 0;
+
+}
